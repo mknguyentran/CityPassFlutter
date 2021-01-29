74535cc24a82b5227cd32dcb501b1c5d57c3ef74
Mk <mknguyentran@yahoo.com>
2021-01-29 23:28:14 +0700

    implemented bottome nav bar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,18 +1,95 @@
-import 'package:CityPass/shared/custom_nav_bar.dart';
+import 'package:CityPass/screens/home/components/history.dart';
+import 'package:CityPass/screens/home/components/location.dart';
+import 'package:CityPass/screens/home/components/my_passes.dart';
+import 'package:CityPass/screens/home/components/passes.dart';
+import 'package:CityPass/shared/custom-nav-bar.dart';
 import 'package:flutter/material.dart';
-import 'package:CityPass/screens/home/components/body.dart';
+import 'package:CityPass/screens/home/components/featured.dart';
 import 'package:CityPass/constants.dart';
 import 'package:CityPass/size_config.dart';
 
-class HomeScreen extends StatelessWidget {
+// class HomeScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     SizeConfig().init(context);
+//     return Scaffold(
+//       extendBodyBehindAppBar: true,
+//       appBar: buildAppBar(),
+//       body: Featured(),
+//       bottomNavigationBar: CustomNavBar(
+//         children: [
+//           CustomNavItem(icon: Icons.star, label: "Đề xuất"),
+//           CustomNavItem(icon: Icons.location_on, label: "Địa điểm"),
+//           CustomNavItem(icon: Icons.qr_code),
+//           CustomNavItem(icon: Icons.local_activity_rounded, label: "CityPass"),
+//           CustomNavItem(icon: Icons.history, label: "Lịch sử"),
+//         ],
+//         focusedIndex: 2,
+//       ),
+//     );
+//   }
+
+//   AppBar buildAppBar() {
+//     return AppBar(
+//       backgroundColor: Colors.transparent,
+//       brightness: Brightness.dark,
+//       title: Row(
+//         children: [
+//           Text(
+//             'CityPass tại TP. Hồ Chí Minh',
+//           ),
+//           IconButton(
+//             icon: Icon(
+//               Icons.arrow_drop_down,
+//               color: kWhiteIconColor,
+//             ),
+//             onPressed: () {},
+//           )
+//         ],
+//       ),
+//       actions: [
+//         IconButton(
+//             icon: ClipOval(child: Image.asset("assets/images/profile.png")),
+//             onPressed: () {})
+//       ],
+//     );
+//   }
+// }
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static List<Widget> tabs = [Featured(), Locations(), MyPasses(), Passes(), History()];
+  int _currentIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: buildAppBar(),
-      body: Body(),
-      bottomNavigationBar: CustomNavBar(),
+      body: tabs.elementAt(_currentIndex),
+      bottomNavigationBar: CustomNavBar(
+        currentIndex: _currentIndex,
+        children: [
+          CustomNavItem(icon: Icons.star, label: "Đề xuất"),
+          CustomNavItem(icon: Icons.location_on, label: "Địa điểm"),
+          CustomNavItem(icon: Icons.qr_code),
+          CustomNavItem(icon: Icons.local_activity_rounded, label: "CityPass"),
+          CustomNavItem(icon: Icons.history, label: "Lịch sử"),
+        ],
+        focusedIndex: 2,
+        onChange: _onItemTapped,
+      ),
     );
   }
 
