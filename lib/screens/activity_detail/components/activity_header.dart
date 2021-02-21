3a87f7e8dce4d7a43d90f507bb3e5a3a77d1be24
dangkhoaph <dangkhoaphoff@gmail.com>
2021-02-21 22:09:44 +0700

    Activity info (basic, without logic)

--- lib/screens/activity_detail/components/activity_header.dart
+++ lib/screens/activity_detail/components/activity_header.dart
@@ -1,4 +1,3 @@
-import 'package:city_pass/constants.dart';
 import 'package:city_pass/models/activity.dart';
 import 'package:city_pass/size_config.dart';
 import 'package:flutter/material.dart';
@@ -23,14 +22,13 @@ class ActivityHeader extends StatelessWidget {
         Positioned(
             bottom: 0,
             child: Container(
-              padding: EdgeInsets.all(
-                getProportionateScreenWidth(kDefaultPadding),
-              ),
+              padding: EdgeInsets.fromLTRB(15,10,15,10),
               height: 100,
               width: MediaQuery.of(context).size.width,
               color: Colors.black.withOpacity(0.6),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
                     activity.name,
@@ -38,6 +36,42 @@ class ActivityHeader extends StatelessWidget {
                         fontSize: 24,
                         fontWeight: FontWeight.bold,
                         color: Colors.white),
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.circle,
+                        color: Colors.green,
+                        size: 18,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 5),
+                        child: Text(
+                          "Đang hoạt động",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_on_rounded,
+                        color: Colors.yellow,
+                        size: 18,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 5),
+                        child: Text(
+                          "Địa chỉ",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
--- lib/screens/activity_detail/components/body/activity_info.dart
+++ lib/screens/activity_detail/components/body/activity_info.dart
@@ -1,10 +1,129 @@
+import 'package:city_pass/constants.dart';
+import 'package:city_pass/models/activity.dart';
+import 'package:city_pass/size_config.dart';
 import 'package:flutter/material.dart';
 
+//Replace with activity data
+String test = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+String image = "assets/images/dinh_doc_lap.jpg";
+
 class ActivityInfo extends StatelessWidget {
+  final Activity activity;
+  const ActivityInfo({
+    Key key,
+    @required this.activity,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Thông tin"),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              child: Text(
+                test, //Replace with activity description
+                style: TextStyle(
+                  height: 1.4),
+              ),
+            ),
+          ),
+          VerticalSpacing(
+            of: 50
+          ),
+          SingleChildScrollView(
+            clipBehavior: Clip.none,
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ...List.generate( //Replace with activity images
+                  2,
+                  (index) => Padding(
+                    padding: index != 0 ? EdgeInsets.only(left: 20) : EdgeInsets.zero,
+                    child: Image.asset(
+                      image,
+                      width: getProportionateScreenWidth(350),
+                      fit: BoxFit.cover
+                    ),
+                  )
+                )
+              ],
+            ),
+          ),
+          VerticalSpacing(
+            of: 50
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Thời gian hoạt động",
+                            style: TextStyle(
+                              fontSize: 21,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  VerticalSpacing(of: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Thứ hai")
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("09:00 - 11:30 và 11:30 - 19:00")
+                        ],
+                      ),
+                    ],
+                  ),
+                  VerticalSpacing(of: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Thứ ba", style: TextStyle(color: primaryDarkColor),)
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("09:00 - 11:30 và 11:30 - 19:00", style: TextStyle(color: primaryDarkColor),)
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
