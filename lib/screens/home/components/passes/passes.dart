f54dc3d612efb51ff6dcd1b025ced6e1ad42af3f
dangkhoaph <dangkhoaphoff@gmail.com>
2021-03-01 13:23:50 +0700

    Current location's CityPass

--- lib/screens/home/components/passes/passes.dart
+++ lib/screens/home/components/passes/passes.dart
@@ -1,4 +1,11 @@
+import 'package:city_pass/constants.dart';
 import 'package:city_pass/models/city.dart';
+import 'package:city_pass/models/pass.dart';
+import 'package:city_pass/screens/home/components/featured/search_field.dart';
+import 'package:city_pass/screens/pass_detail/pass_detail.dart';
+import 'package:city_pass/shared/pass_card.dart';
+import 'package:city_pass/size_config.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Passes extends StatelessWidget {
@@ -8,6 +15,42 @@ class Passes extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Container(alignment: Alignment.center, child: Text("Passes Tab"));
+    return Container(
+      alignment: Alignment.topCenter,
+      padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 70),
+              child: SearchField(
+                hintText: "Tìm kiếm CityPass",
+                boxShadow: [kPopShadow],
+              ),
+            ),
+            VerticalSpacing(of: 20),
+            ...List.generate(
+              mockupPasses.length,
+              (index) => Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: PassCard(
+                  pass: mockupPasses[index],
+                  press: () {
+                    Navigator.push(
+                      context,
+                      CupertinoPageRoute(builder: (context) {
+                        return PassDetail(
+                          pass: mockupPasses[index],
+                        );
+                      })
+                    );
+                  }
+                ),
+              )
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
