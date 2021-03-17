a9a9df4aeef71985c227509ce15892b58cd4a8b2
dangkhoaph <dangkhoaphoff@gmail.com>
2021-03-17 23:47:03 +0700

    no message

--- lib/screens/home/components/featured/explore.dart
+++ lib/screens/home/components/featured/explore.dart
@@ -24,81 +24,93 @@ class Explore extends StatefulWidget {
 }
 
 class _ExploreState extends State<Explore> {
-  List<TicketType> listRealActivitiesNearYou_3;
+  Future<List<TicketType>> listRealActivitiesNearYou_3;
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      listRealActivitiesNearYou_3 =
-          await TicketTypeAPI().getAllTicketTypes(onError: (msg) {
-        print("Mistake: " + msg);
-      });
+    // WidgetsBinding.instance.addPostFrameCallback((_) async {
+    //   listRealActivitiesNearYou_3 =
+    //       await TicketTypeAPI().getAllTicketTypes(onError: (msg) {
+    //     print("Mistake: " + msg);
+    //   });
+    // });
+    listRealActivitiesNearYou_3 =
+        TicketTypeAPI().getAllTicketTypes(onError: (msg) {
+      print(msg);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     var verticalSpacing = 60.0;
-    return SingleChildScrollView(
-      clipBehavior: Clip.none,
-      child: Column(
-        children: [
-          Padding(
-            padding:
-                EdgeInsets.symmetric(horizontal: kDefaultPadding, vertical: 20),
-            child: Container(
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [kDefaultShadow]),
-              child: Image(
-                image: AssetImage("assets/images/tiniworld.png"),
-                fit: BoxFit.fill,
+    return FutureBuilder(
+        future: listRealActivitiesNearYou_3,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return SingleChildScrollView(
+              clipBehavior: Clip.none,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: kDefaultPadding, vertical: 20),
+                    child: Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [kDefaultShadow]),
+                      child: Image(
+                        image: AssetImage("assets/images/tiniworld.png"),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                  AttractionCategory(),
+                  VerticalSpacing(
+                    of: verticalSpacing,
+                  ),
+                  PassRecommendation(
+                    title: "Combo được yêu thích",
+                    children: mockupPasses,
+                  ),
+                  VerticalSpacing(
+                    of: verticalSpacing,
+                  ),
+                  PassRecommendation(
+                    title: "Deal tốt Tháng 3 🎉",
+                    subtitle: "Giảm đến 50% khi mua trong tháng này",
+                    children: mockupPasses,
+                  ),
+                  VerticalSpacing(
+                    of: verticalSpacing,
+                  ),
+                  ActivityRecommendation(
+                    title: "Gần bạn nhất",
+                    // children: mockupNearYouActivities_3,
+                    children: snapshot.data,
+                  ),
+                  VerticalSpacing(
+                    of: verticalSpacing,
+                  ),
+                  // ActivityRecommendation(
+                  //   title: "Điểm đến nổi bật",
+                  //   children: listRealActivitiesNearYou_3,
+                  // ),
+                  // ActivityRecommendationVertical(
+                  //   children: List.generate(30, (index) {
+                  //     var ran = new Random();
+                  //     var ranInt = ran.nextInt(mockupActivities.length - 1);
+                  //     return mockupActivities[ranInt];
+                  //   }),
+                  // ),
+                  VerticalSpacing(
+                    of: verticalSpacing,
+                  ),
+                ],
               ),
-            ),
-          ),
-          AttractionCategory(),
-          VerticalSpacing(
-            of: verticalSpacing,
-          ),
-          PassRecommendation(
-            title: "Combo được yêu thích",
-            children: mockupPasses,
-          ),
-          VerticalSpacing(
-            of: verticalSpacing,
-          ),
-          PassRecommendation(
-            title: "Deal tốt Tháng 3 🎉",
-            subtitle: "Giảm đến 50% khi mua trong tháng này",
-            children: mockupPasses,
-          ),
-          VerticalSpacing(
-            of: verticalSpacing,
-          ),
-          ActivityRecommendation(
-            title: "Gần bạn nhất",
-            // children: mockupNearYouActivities_3,
-            children: listRealActivitiesNearYou_3,
-          ),
-          VerticalSpacing(
-            of: verticalSpacing,
-          ),
-          ActivityRecommendation(
-            title: "Điểm đến nổi bật",
-            children: listRealActivitiesNearYou_3,
-          ),
-          // ActivityRecommendationVertical(
-          //   children: List.generate(30, (index) {
-          //     var ran = new Random();
-          //     var ranInt = ran.nextInt(mockupActivities.length - 1);
-          //     return mockupActivities[ranInt];
-          //   }),
-          // ),
-          VerticalSpacing(
-            of: verticalSpacing,
-          ),
-        ],
-      ),
-    );
+            );
+          }
+
+          return CircularProgressIndicator();
+        });
   }
 }
