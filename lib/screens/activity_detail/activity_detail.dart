88d67d8b13dab385459d0cfa3baa1cf89000372e
MinhKhoa <mknguyentran@yahoo.com>
2021-03-23 15:54:45 +0700

    add home button to activity detail, pass detail, and userpass detail

--- lib/screens/activity_detail/activity_detail.dart
+++ lib/screens/activity_detail/activity_detail.dart
@@ -5,6 +5,7 @@ import 'package:city_pass/screens/activity_detail/components/body/activity_body.
 import 'package:city_pass/models/ticketType.dart';
 import 'package:city_pass/service/ticketType_services.dart';
 import 'package:city_pass/size_config.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_guid/flutter_guid.dart';
 
@@ -40,24 +41,26 @@ class _ActivityDetailState extends State<ActivityDetail> {
       extendBodyBehindAppBar: true,
       appBar: _buildAppBar(context),
       body: SingleChildScrollView(
-          clipBehavior: Clip.none,
-          child: FutureBuilder(
-              future: ticketTypeDetail,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Column(
-                    children: [
-                      ActivityHeader(activity: snapshot.data),
-                      ActivityBody(activity: snapshot.data),
-                    ],
-                  );
-                }
-                return Container(
-                  height: percentageOfScreenHeight(100),
-                  alignment: Alignment.center,
-                  child: CircularProgressIndicator(),
-                );
-              })),
+        clipBehavior: Clip.none,
+        child: FutureBuilder(
+          future: ticketTypeDetail,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                children: [
+                  ActivityHeader(activity: snapshot.data),
+                  ActivityBody(activity: snapshot.data),
+                ],
+              );
+            }
+            return Container(
+              height: percentageOfScreenHeight(100),
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+      ),
     );
   }
 
@@ -75,7 +78,12 @@ class _ActivityDetailState extends State<ActivityDetail> {
         },
       ),
       actions: [
-        IconButton(icon: Icon(Icons.favorite_border_rounded), onPressed: () {})
+        IconButton(
+          icon: Icon(CupertinoIcons.home),
+          onPressed: () {
+            Navigator.of(context).popUntil((route) => route.isFirst);
+          },
+        )
       ],
     );
   }
